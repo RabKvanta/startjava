accbe265dd5184b9504ee5588a7c29c6b2358fc0
RabKvanta <37476810+RabKvanta@users.noreply.github.com>
2020-02-20 20:35:16 +0200

    Variant Unicode

--- Lesson_01/src/Unicode.java
+++ Lesson_01/src/Unicode.java
@@ -1,12 +1,14 @@
 public class Unicode {
     public static void main(String[] args) {
-        char ch = '\u9398';
-        for (int i = 9398; i <= 10178; i++) {
-            System.out.println(ch++);
-        }
-        System.out.println();
-        for (ch = '\u0033'; ch <= '\u0126'; ch++) {
+
+        for (char ch = 33; ch <= 126; ch++) {
             System.out.println(ch);
         }
+
+        System.out.println("============================");
+
+          for (char ch=9398; ch <= 10178; ch++) {
+              System.out.println(ch);
+          }
     }
 }
