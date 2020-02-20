d967ad1ea51e98c21a91fbb8905d6bc277cac6be
RabKvanta <37476810+RabKvanta@users.noreply.github.com>
2020-02-21 00:41:36 +0200

    Variant Unicode '\u0021'

--- Lesson_01/src/Unicode.java
+++ Lesson_01/src/Unicode.java
@@ -2,13 +2,20 @@ public class Unicode {
     public static void main(String[] args) {
 
         for (char ch = 33; ch <= 126; ch++) {
-            System.out.println(ch);
+            System.out.print(ch+" ");
         }
 
-        System.out.println("============================");
+        System.out.println();
 
+        for (char ch = '\u0021'; ch <= '\u007E'; ch++) {
+            System.out.print(ch+" ");
+        }
+        System.out.println();
+/*
           for (char ch=9398; ch <= 10178; ch++) {
               System.out.println(ch);
           }
+
+ */
     }
 }
